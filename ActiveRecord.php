70160393939cd993ddf8cb6cc526f4ec7d26c050
Emilio Silveira <emilio.rst@gmail.com>
2009-11-26 22:55:58 -0430

    Añadiendo una base

---
+++ ActiveRecord.php
@@ -0,0 +1,6 @@
+<?php
+
+// de momento para que nos permita cargar el combo :)
+
+require CORE_PATH . 'ActiveRecord/db_pool/db_pool.php';
+require CORE_PATH . 'ActiveRecord/active_record2/active_record2.php';
---
+++ active_record2/active_record2.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * KumbiaPHP web & app Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://wiki.kumbiaphp.com/Licencia
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@kumbiaphp.com so we can send you a copy immediately.
+ *
+ * Implementacion del patron de diseño ActiveRecord
+ * 
+ * @category   Kumbia
+ * @package    ActiveRecord
+ * @copyright  Copyright (c) 2005-2009 Kumbia Team (http://www.kumbiaphp.com)
+ * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
+ */
+ 
+/**
+ * ActiveRecord Clase para el Mapeo Objeto Relacional
+ *
+ * Active Record es un enfoque al problema de acceder a los datos de una
+ * base de datos en forma orientada a objetos. Una fila en la
+ * tabla de la base de datos (o vista) se envuelve en una clase,
+ * de manera que se asocian filas únicas de la base de datos
+ * con objetos del lenguaje de programación usado.
+ * Cuando se crea uno de estos objetos, se añade una fila a
+ * la tabla de la base de datos. Cuando se modifican los atributos del
+ * objeto, se actualiza la fila de la base de datos.
+ */
+class ActiveRecord2
+{
+
+}
