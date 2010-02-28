f225a513c2be0d25b588851eb448ec39f259278e
Emilio Silveira <emilio.rst@gmail.com>
2010-02-28 09:38:13 -0430

    Adicionando metodo isAttribute

--- db_pool/metadata.php
+++ db_pool/metadata.php
@@ -127,4 +127,15 @@ class Metadata
     {
         return $this->_FK;
     }
+	
+	/**
+	 * Verifica que sea un atributo valido
+	 * 
+	 * @param string $attribute
+	 * @return boolean
+	 */
+	public function isAttribute($attribute)
+	{
+		return isset($this->_attributes[$attribute]);
+	}
 }
