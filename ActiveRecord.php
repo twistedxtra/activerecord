b1fe9f8ee060da1a7e9c7dfd2207830a0504bf9c
Manuel Aguirre <programador.manuel@gmail.com>
2012-06-16 15:15:29 -0430

    cambio en las rutas para los require, asi no está atado a una ruta

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -2,5 +2,5 @@
 
 // de momento para que nos permita cargar el combo :)
 
-require CORE_PATH . 'libs/ActiveRecord/db_pool/db_pool.php';
-require CORE_PATH . 'libs/ActiveRecord/active_record2/active_record2.php';
+require_once 'db_pool/db_pool.php';
+require_once 'active_record2/active_record2.php';
