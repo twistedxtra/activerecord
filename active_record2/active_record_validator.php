9915393a9d890471091d349a9a8b2d1894c428ea
Manuel Aguirre <programador.manuel@gmail.com>
2012-06-16 15:38:27 -0430

    formato en la doc, y cambio en la obtencion de las validaciones
    ahora las validaciones estan contenidas en un objeto validations en el
    modelo, el cual contiene un arreglo con las validaciones.
    
    se simplifican los foreach ya que siempre el indice del arreglo será
    el nombre del campo, y el valor de esa posicion los parametros.

--- active_record2/active_record_validator.php
+++ active_record2/active_record_validator.php
@@ -19,171 +19,137 @@
  * @copyright  Copyright (c) 2005-2010 Kumbia Team (http://www.kumbiaphp.com)
  * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
-
 // @see ValidatorInterface
-require_once CORE_PATH . 'libs/ActiveRecord/active_record2/validators/validator_interface.php';
+require_once 'validators/validator_interface.php';
 
 class ActiveRecordValidator
-{	
-	/**
-	 * Validar en caso de crear
-	 * 
-	 * @param ActiveRecord $object
-	 * @return boolean
-	 */
-	public final static function validateOnCreate($object)
-	{
-		return self::_validate($object);
-	}
-	
-	/**
-	 * Validar en caso de actualizar
-	 * 
-	 * @param ActiveRecord $object
-	 * @return boolean
-	 */
-	public static function validateOnUpdate($object)
-	{
-		return self::_validate($object, TRUE);
-	}
-	
-	/**
-	 * Efectua las validaciones
-	 * 
-	 * @param ActiveRecord $object
-	 * @param boolean $update
-	 * @return boolean
-	 */
-	private static function _validate($object, $update = FALSE)
-	{
-		// Obtiene los validadores
-		$validators = $object->validators();
-		
-		// Si no hay validadores definidos
-		if(!$validators) {
-			return TRUE;
-		}
-				
-		// Columnas con valor por defectos
-		$default = array();
-		
-		// Verifica si existe columnas con valor por defectos
-		if(isset($validators['default'])) {
-			// @see DefaultValidator
-			require_once CORE_PATH . "libs/ActiveRecord/active_record2/validators/default_validator.php";
-			
-			// Itera en cada definicion de validacion
-			foreach($validators['default'] as $v) {
-				// Verifica las condiciones para cuando la columna es con valor por defecto
-				$default[$v] = DefaultValidator::validate($object, $v, NULL, $update);
-			}
-			
-			// Aprovecha y libera memoria :)
-			unset($validators['default']);
-		}
-		
-		// Por defecto es valido
-		$valid = TRUE;
-		
-		// Verifica si existe columnas no nulas
-		if(isset($validators['notNull'])) {
-			// @see NotNullValidator
-			require_once CORE_PATH . "libs/ActiveRecord/active_record2/validators/not_null_validator.php";
-			
-			// Itera en cada definicion de validacion
-			foreach($validators['notNull'] as $v) {
-				// Si es una columna sin configuracion especial
-				if(is_string($v)) {
-					// Si es con valor por defecto entonces salta la validacion
-					if(isset($default[$v]) && $default[$v]) {
-						continue;
-					}
-					
-					$column = $v;					
-					$params = NULL;
-				} else {
-					// Si es con valor por defecto entonces salta la validacion
-					if(isset($default[$v[0]]) && $default[$v[0]]) {
-						continue;
-					}
-					
-					$column = $v[0];
-					unset($v[0]);
-					$params = $v;
-				}
-				
-				// Valida si el campo
-				$valid = NotNullValidator::validate($object, $column, $params, $update) && $valid;
-			}
-			
-			// Aprovecha y libera memoria :)
-			unset($validators['notNull']);
-		}
-		
-		// Realiza el resto de las validaciones a las columnas
-		foreach($validators as $validator => $validations) {
-			// Clase validadora
-			$class = $validator.'validator';
-			
-			// Carga el validador de ser necesario
-			if(!class_exists($class, FALSE)) {
-				self::_load($validator);
-			}
-			
-			// Itera en cada definicion de validacion
-			foreach($validations as $v) {
-				
-				// Si es una columna sin configuracion especial
-				if(is_string($v)) {
-					// Si es con valor por defecto entonces salta la validacion
-					if(isset($default[$v]) && $default[$v]) {
-						continue;
-					}
+{
 
-					$column = $v;					
-					$params = NULL;
-				} else {
-					// Si es con valor por defecto entonces salta la validacion
-					if(is_string($v[0]) && isset($default[$v[0]]) && $default[$v[0]]) {
-						continue;
-					}
-					
-					$column = $v[0];
-					unset($v[0]);
-					$params = $v;
-				}
-								
-				if(is_array($column) || (isset($object->$column) && $object->$column != '')) {
-					$valid = call_user_func(array($class, 'validate'), $object, $column, $params, $update) && $valid;
-				}
-				
-			}
-			
-		}
-		
-		// Resultado de validacion
-		return $valid;
-	}
-	
-	/**
-	 * Carga un validador
-	 *
-	 * @param string $validator
-	 * @throw KumbiaException
-	 */
-	private static function _load($validator)
-	{
-		// Convierte a smallcase
-		$validatorSmall = Util::smallcase($validator);
-		
-		$file = APP_PATH . "extensions/validators/{$validatorSmall}_validator.php";
-		if(!is_file($file)) {
-			$file = CORE_PATH . "libs/ActiveRecord/active_record2/validators/{$validatorSmall}_validator.php";
-			if(!is_file($file)) {
-				throw new KumbiaException("Validador $validator no encontrado");
-			}
-		}
-        
-        include $file;
-	}
+    /**
+     * Validar en caso de crear
+     * 
+     * @param ActiveRecord $object
+     * @return boolean
+     */
+    public final static function validateOnCreate($object)
+    {
+        return self::_validate($object);
+    }
+
+    /**
+     * Validar en caso de actualizar
+     * 
+     * @param ActiveRecord $object
+     * @return boolean
+     */
+    public static function validateOnUpdate($object)
+    {
+        return self::_validate($object, TRUE);
+    }
+
+    /**
+     * Efectua las validaciones
+     * 
+     * @param ActiveRecord $object
+     * @param boolean $update
+     * @return boolean
+     */
+    private static function _validate($object, $update = FALSE)
+    {
+        // Obtiene los validadores
+        $validators = $object->validations()->getValidations();
+
+        // Si no hay validadores definidos
+        if (!$validators) {
+            return TRUE;
+        }
+
+        // Columnas con valor por defectos
+        $default = array();
+
+        // Verifica si existe columnas con valor por defectos
+        if (isset($validators['default']) && count($validators['default'])) {
+            // @see DefaultValidator
+            require_once "validators/default_validator.php";
+
+            // Itera en cada definicion de validacion
+            foreach ($validators['default'] as $field => $params) {
+                // Verifica las condiciones para cuando la columna es con valor por defecto
+                $default[$field] = DefaultValidator::validate($object, $field, $params, $update);
+            }
+
+            // Aprovecha y libera memoria :)
+            unset($validators['default']);
+        }
+
+        // Por defecto es valido
+        $valid = TRUE;
+
+        // Verifica si existe columnas no nulas
+        if (isset($validators['notNull']) && count($validators['notNull'])) {
+            // @see NotNullValidator
+            require_once "validators/not_null_validator.php";
+
+            // Itera en cada definicion de validacion
+            foreach ($validators['notNull'] as $column => $params) {
+                // Si es con valor por defecto entonces salta la validacion
+                if (isset($default[$column]) && $default[$column]) {
+                    continue;
+                }
+                // Valida si el campo
+                $valid = NotNullValidator::validate($object, $column, $params, $update) && $valid;
+            }
+        }
+
+        // Aprovecha y libera memoria :)
+        unset($validators['notNull']);
+
+        // Realiza el resto de las validaciones a las columnas
+        foreach ($validators as $validator => $validations) {
+            // Clase validadora
+            $class = $validator . 'validator';
+
+            // Carga el validador de ser necesario
+            if (!class_exists($class, FALSE)) {
+                self::_load($validator);
+            }
+
+            // Itera en cada definicion de validacion
+            foreach ($validations as $column => $params) {
+                if (isset($default[$column]) && $default[$column]) {
+                    continue;
+                }
+
+                if (isset($object->$column) && $object->$column != '') {
+                    $valid = call_user_func(array($class, 'validate'), $object, $column, $params, $update) && $valid;
+                }
+            }
+        }
+
+        // Resultado de validacion
+        return $valid;
+    }
+
+    /**
+     * Carga un validador
+     *
+     * @param string $validator
+     * @throw KumbiaException
+     */
+    private static function _load($validator)
+    {
+        // Convierte a smallcase
+        $validatorSmall = Util::smallcase($validator);
+
+        $file = APP_PATH . "extensions/validators/{$validatorSmall}_validator.php";
+        if (!is_file($file)) {
+            $file = __DIR__ . "/validators/{$validatorSmall}_validator.php";
+            if (!is_file($file)) {
+                throw new KumbiaException("Validador $validator no encontrado");
+            }
+        }
+
+        include_once $file;
+    }
+
 }
