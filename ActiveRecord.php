a1287a48368fa4d7d36dcf8654202d52bcbec343
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-30 20:29:35 -0430

    no se usa ya este archivo

--- ActiveRecord.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// de momento para que nos permita cargar el combo :)
-
-require CORE_PATH . 'libs/ActiveRecord/db_pool/db_pool.php';
-require CORE_PATH . 'libs/ActiveRecord/active_record2/active_record2.php';
