1b643068589adc153a84999765aa337dbb2ce752
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-30 20:30:33 -0430

    clase para excepciones del AR

---
+++ Exception/ActiveRecordException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ActiveRecord\Model;
+
+use \Exception;
+
+/**
+ * Description of ActiveRecordException
+ *
+ * @author maguirre
+ */
+class ActiveRecordException extends Exception
+{
+    
+}
+
